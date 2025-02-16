308f3dcacaf0bab7dad30607f8a4f695134f2247
Alex Morgan <lexyym@gmail.com>
2025-02-16 15:58:10 -0500

    tables of values

--- src/ViewGroup.jsx
+++ src/ViewGroup.jsx
@@ -1,18 +1,47 @@
 import { useURL } from './URLContext';
+import { allKeysForVariables, valueForFeature } from './variables.js';
 
-export default function ViewGroup({group}) {
+export default function ViewGroup({group, variables}) {
+    let keys = allKeysForVariables(variables);
     return (
-	<p>
+	<div>
 	    <b className="thingname"> { group.name } </b>
 	    <table>
-		{ group.subjects.map(
-		    (s) => <tr key={s.id} >
+		<thead>
+		    <tr>
+			<th>Subject ID</th>
+			{ keys.map(
+			    (k) => <th key={k}>{k}</th>) }
+		    </tr>
+		</thead>
+		<tbody>
+	   	  { group.subjects.map(
+		      (s) => <tr key={s.id} >
 			       <td>
 			           {s.id}
 			       </td>
+				 {  keys.map(
+				     (v) => <td key={v}>
+						{valueForFeature(s.features,v)}
+					     </td>)  
+				 }
 			   </tr>) }
+		</tbody>
+		<tfoot>
+		    { group.meanVector && (
+		    <tr>
+			<th> means: </th>
+			{ keys.map(
+			    (v) => <td key={v}>
+				       <b>
+					   {valueForFeature(group.meanVector,v)}
+					   </b>
+				   </td>)
+			}
+		    </tr>) }
+		    
+		</tfoot>
 	    </table>
-		{ JSON.stringify(group) }
-	</p>
+	</div>
     )
 }
--- src/ViewProtocol.jsx
+++ src/ViewProtocol.jsx
@@ -6,10 +6,12 @@ import ViewVariables from './ViewVariables';
 export default function ViewProtocol({name, protocol}) {
     const randomizerURL = useURL();
     const [groupList, setGroupList] = useState([]);
+    const [subjectList, setSubjectList] = useState([]);
     const [varNameList, setVarNameList] = useState([]);
 
     useEffect(() => {
 	getGroups();
+	getSubjects();
     }, [randomizerURL, name]);
 
     async function getGroups() {
@@ -30,15 +32,41 @@ export default function ViewProtocol({name, protocol}) {
 	}
     }
 
+    async function getSubjects() {
+	let url = `${randomizerURL}/${name}/subjects`;
+	try {
+	    const response = await fetch(url);
+	    if (!response.ok) {
+		// TODO: deal with errors
+	    }
+	    else {
+		const newSubjectList = await response.json();
+		console.log("Subjects", newSubjectList);
+		setSubjectList(newSubjectList);
+	    }
+	}
+	catch {
+	    // TODO: deal with errors
+	}
+    }
+
     return (
 	<>
 	    <h2> Protocol: {name}</h2>
 	    <ViewVariables variables={protocol.variables} />
 	    <h3>Groups:</h3>
 	    { groupList.map( (g) =>
-		<ViewGroup group={g} key={g.name} />
+		<ViewGroup group={g} variables={protocol.variables}
+			   key={g.name} />
 	    )}
+	    <h3>Unassigned subjects:</h3>
+	    <p>
+		{ subjectList.filter(s => s.groupName == null).map(
+		    s => <b>{s.id}</b> )
+	    }
+	    </p>
 	    <hr/>
+	    {JSON.stringify(subjectList)}
 	</>
     )
 }
