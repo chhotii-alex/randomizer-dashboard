886d7f8017a5f9936be193f94662abf16514c37f
chhotii-alex <lexyym@gmail.com>
2025-02-18 14:49:22 -0500

    validation and starting on reducer

--- src/NewProtocolPanel.jsx
+++ src/NewProtocolPanel.jsx
@@ -1,4 +1,4 @@
-import {useState, useContext } from 'react';
+import {useState, useContext, useReducer } from 'react';
 import { useURL } from './URLContext';
 import RefreshContext from './RefreshContext.js';
 
@@ -6,103 +6,164 @@ export default function NewProtocolPanel({cancelAdding}) {
     const randomizerURL = useURL();
     const refreshFunction = useContext(RefreshContext);
     const [name, setName] = useState('Intervention_Trial');
-    const [specs, setSpecs] = useState({
+    const [specs, dispatch] = useReducer(reducer, {
 	algorithm: 'Balanced',
 	allowRevision: true,
 	groupNames: ["A", "B"],
 	variableSpec: [{name: "score", type: "continuous", levels:[]}]
     });
+    const errorMessage = validate();
+
+    function validate() {
+	if (!name || name.length < 1) {
+	    return "name of protocol must be non-empty";
+	}
+	if (name.includes(' ')) return "name of protocol cannot contain spaces";
+	for (const group of specs.groupNames) {
+	    if (!group || group.length < 1) {
+		return "group name cannot be empty";
+	    }
+	}
+	for (const v of specs.variableSpec) {
+	    console.log(v);
+	    if (!v.name || v.name.length < 1) {
+		return "variable name cannot be empty";
+	    }
+	    if (v.type == "categorical") {
+		if (!v.levels || v.levels.length < 2) {
+		    return "categorical variables should have at least 2 levels"
+		}
+		for (const levelText of v.levels) {
+		    if (!levelText || levelText.length < 1) {
+			return "a categorical level's label cannot be empty";
+		    }
+		}
+		const asSet = new Set(v.levels);
+		if (asSet.size < v.levels.length) {
+		    return "level labels must be distinct";
+		}
+	    }
+	}
+	
+	// all tests passed
+	return null;
+    }
 
     function handleNameChange(e) {
-	setName(e.target.value);
+	let s = e.target.value;
+	setName(s.replace(/\s/g, ''));
+    }
+
+    function reducer(specs, action) {
+	if (action.type === 'changed') {
+	    return {...specs, action.field:action.value};
+	}
+	if (action.type === 'groupName') {
+	    const newGroups = specs.groupNames.slice();
+	    newGroups.splice(action.index, 1, action.value);
+	    return {...specs, groupNames:newGroups};
+	}
+	if (action.type === 'addGroup') {
+	    const newGroups = specs.groupNames.slice();
+	    newGroups.push('new group name');
+	    return {...specs, groupNames:newGroups};
+	}
+	if (action.type === 'removeGroup') {
+	    const newGroups = specs.groupNames.slice();
+	    newGroups.splice(action.index, 1);
+	    return {...specs, groupNames:newGroups};
+	}
+	if (action.type === 'updateVariable') {
+	    const newVariables = specs.variableSpec.slice();
+	    const newVariable = {...newVariables[action.index], action.field:action.value};
+	    if (newVariable.type == "categorical") {
+		let counter = 1;
+		while (newVariable.levels.length < 2) {
+		    newVariable.levels.push(`level ${counter}`);
+		    counter += 1;
+		}
+	    }
+	    newVariables.splice(action.index, 1, newVariable);
+	    return {...specs, variableSpec:newVariables};
+	}
+	if (action.type === 'addVariable') {
+	    const newVariable = {name:"new_var", type:"continuous", levels:[]};
+	    const newVariables = specs.variableSpec.slice();
+	    newVariables.push(newVariable);
+	    return{...specs, variableSpec:newVariables};
+	}
+	if (action.type === 'removeVariable') {
+	    const newVariables = specs.variableSpec.slice();
+	    newVariables.splice(action.index, 1);
+	    return {...specs, variableSpec:newVariables};
+	}
+	if (action.type == 'addLevel') {
+	    const newVariables = specs.variableSpec.slice();
+	    const newVariable = {...newVariables[action.index]};
+	    newVariable.levels.push('level');
+	    newVariables.splice(action.index, 1, newVariable);
+	    return {...specs, variableSpec:newVariables};
+	}
+	if (action.type == 'changeLevel') {
+	    const newLevels = specs.variableSpec[action.variableIndex].levels.slice();
+	    newLevels.splice(action.levelIndex, 1, action.value);
+	    const newVariable = {...specs.variableSpec[action.variableIndex], levels:newLevels}
+	    const newVariables = specs.variableSpec.slice();
+	    newVariables.splice(action.variableIndex, 1, newVariable);
+	    return {...specs, variableSpec:newVariables};
+	}
+	if (action.type === 'removeLevel') {
+	    const newLevels = specs.variableSpec[action.variableIndex].levels.slice();
+	    newLevels.splice(action.levelIndex, 1);
+	    const newVariable = {...specs.variableSpec[action.variableIndex], levels:newLevels}
+	    const newVariables = specs.variableSpec.slice();
+	    newVariables.splice(action.variableIndex, 1, newVariable);
+	    return {...specs, variableSpec:newVariables};
+	}
     }
 
     function handleAlgorithmChange(e) {
-	setSpecs({...specs, algorithm:e.target.value});
+	dispatch({type:'changed', field:'algorithm', value:e.target.value});
     }
 
     function handleAllowRevisionsChange(e) {
-	const newSpecs = {...specs, allowRevision:e.target.checked};
-	setSpecs(newSpecs);
+	dispatch({type:'changed', field:'allowRevision', value:e.target.checked});
     }
 
     function updateGroupName(e, i) {
-	const newGroups = specs.groupNames.slice();
-	newGroups.splice(i, 1, e.target.value);
-	const newSpecs = {...specs, groupNames:newGroups};
-	setSpecs(newSpecs);
+	dispatch({type:'groupName', index:i, value:e.target.value});
     }
 
     function addNewGroup() {
-	const newGroups = specs.groupNames.slice();
-	newGroups.push('new group name');
-	setSpecs({...specs, groupNames:newGroups});
+	dispatch({type:'addGroup'});
     }
 
     function removeGroupName(i) {
-	const newGroups = specs.groupNames.slice();
-	newGroups.splice(i, 1);
-	setSpecs({...specs, groupNames:newGroups});
+	dispatch({type:'removeGroup', index:i});
     }
 
     function updateVariableName(e, i) {
-	const newVariables = specs.variableSpec.slice();
-	const newVariable = {...newVariables[i], name:e.target.value};
-	newVariables.splice(i, 1, newVariable);
-	const newSpecs = {...specs, variableSpec:newVariables};
-	setSpecs(newSpecs);
-    }
-
-    function addNewVariable() {
-	const newVariable = {name:"new_var", type:"continous", levels:[]};
-	const newVariables = specs.variableSpec.slice();
-	newVariables.push(newVariable);
-	const newSpecs = {...specs, variableSpec:newVariables};
-	setSpecs(newSpecs);
-    }
-
-    function removeVariable(i) {
-	const newVariables = specs.variableSpec.slice();
-	newVariables.splice(i, 1);
-	const newSpecs = {...specs, variableSpec:newVariables};
-	setSpecs(newSpecs);
+	dispatch({type:'updateVariable', index:i, field:'name', value:e.target.value});
     }
 
     function setSelectedType(e, i) {
-	const newVariables = specs.variableSpec.slice();
-	const newVariable = {...newVariables[i], type:e.target.value};
-	newVariables.splice(i, 1, newVariable);
-	const newSpecs = {...specs, variableSpec:newVariables};
-	setSpecs(newSpecs);
+	dispatch({type:'updateVariable', index:i, field:'type', value:e.target.value});
+    }
+
+    function removeVariable(i) {
+	dispatch({type:'removeVariable', index:i});
     }
 
     function addLevel(i) {
-	const newVariables = specs.variableSpec.slice();
-	const newVariable = {...newVariables[i]};
-	newVariable.levels.push('level');
-	newVariables.splice(i, 1, newVariable);
-	const newSpecs = {...specs, variableSpec:newVariables};
-	setSpecs(newSpecs);
+	dispatch({type:'addLevel', index:i});
     }
 
     function updateLevel(e, variableIndex, levelIndex) {
-	const newLevels = specs.variableSpec[variableIndex].levels.slice();
-	newLevels.splice(levelIndex, 1, e.target.value);
-	const newVariable = {...specs.variableSpec[variableIndex], levels:newLevels}
-	const newVariables = specs.variableSpec.slice();
-	newVariables.splice(variableIndex, 1, newVariable);
-	const newSpecs = {...specs, variableSpec:newVariables};
-	setSpecs(newSpecs);
+	dispatch({type:'changeLevel', variableIndex:variableIndex, levelIndex:levelIndex, value:e.target.value});
     }
 
     function removeLevel(variableIndex, levelIndex) {
-	const newLevels = specs.variableSpec[variableIndex].levels.slice();
-	newLevels.splice(levelIndex, 1);
-	const newVariable = {...specs.variableSpec[variableIndex], levels:newLevels}
-	const newVariables = specs.variableSpec.slice();
-	newVariables.splice(variableIndex, 1, newVariable);
-	const newSpecs = {...specs, variableSpec:newVariables};
-	setSpecs(newSpecs);
+	dispatch({type:'removeLevel', variableIndex:variableIndex, levelIndex:levelIndex, value:e.target.value});
     }
 
     async function startProtocol() {
@@ -131,6 +192,7 @@ export default function NewProtocolPanel({cancelAdding}) {
     return (
 	<>
 	    <h3> New Protocol Parameters </h3>
+	    <div className="error">{errorMessage}</div>
 	    <label>Name of new protocol:</label>
 	    <input value={name} onChange={handleNameChange} />
 	    <p>
@@ -190,8 +252,10 @@ export default function NewProtocolPanel({cancelAdding}) {
 		}
 		<button onClick={addNewVariable}>Add Another Variable</button>
 	    </div>
-	    <button onClick={startProtocol}>Start</button>
+	    { !errorMessage &&
+	      <button onClick={startProtocol}>Start</button> }
 	    <button onClick={cancelAdding}>Cancel</button>
+	    { JSON.stringify(specs) }
 	</>
     )
 }
