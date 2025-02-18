63c8406898654e2877238e694ff5120ba635742d
Alex Morgan <lexyym@gmail.com>
2025-02-18 17:54:14 -0500

    debugged reducer and calling dispatch directly

--- src/NewProtocolPanel.jsx
+++ src/NewProtocolPanel.jsx
@@ -25,7 +25,6 @@ export default function NewProtocolPanel({cancelAdding}) {
 	    }
 	}
 	for (const v of specs.variableSpec) {
-	    console.log(v);
 	    if (!v.name || v.name.length < 1) {
 		return "variable name cannot be empty";
 	    }
@@ -49,14 +48,11 @@ export default function NewProtocolPanel({cancelAdding}) {
 	return null;
     }
 
-    function handleNameChange(e) {
-	let s = e.target.value;
-	setName(s.replace(/\s/g, ''));
-    }
-
     function reducer(specs, action) {
 	if (action.type === 'changed') {
-	    return {...specs, action.field:action.value};
+	    let newSpecs = {...specs}
+	    newSpecs[action.field] = action.value;
+	    return newSpecs;
 	}
 	if (action.type === 'groupName') {
 	    const newGroups = specs.groupNames.slice();
@@ -75,7 +71,8 @@ export default function NewProtocolPanel({cancelAdding}) {
 	}
 	if (action.type === 'updateVariable') {
 	    const newVariables = specs.variableSpec.slice();
-	    const newVariable = {...newVariables[action.index], action.field:action.value};
+	    const newVariable = {...newVariables[action.index]}
+	    newVariable[action.field] = action.value;
 	    if (newVariable.type == "categorical") {
 		let counter = 1;
 		while (newVariable.levels.length < 2) {
@@ -100,8 +97,10 @@ export default function NewProtocolPanel({cancelAdding}) {
 	if (action.type == 'addLevel') {
 	    const newVariables = specs.variableSpec.slice();
 	    const newVariable = {...newVariables[action.index]};
-	    newVariable.levels.push('level');
-	    newVariables.splice(action.index, 1, newVariable);
+	    const newLevels = newVariable.levels.slice();
+	    newLevels.push('level');
+	    newVariable.levels = newLevels;
+	    newVariables.splice(action.index, 1, newVariable)
 	    return {...specs, variableSpec:newVariables};
 	}
 	if (action.type == 'changeLevel') {
@@ -120,50 +119,6 @@ export default function NewProtocolPanel({cancelAdding}) {
 	    newVariables.splice(action.variableIndex, 1, newVariable);
 	    return {...specs, variableSpec:newVariables};
 	}
-    }
-
-    function handleAlgorithmChange(e) {
-	dispatch({type:'changed', field:'algorithm', value:e.target.value});
-    }
-
-    function handleAllowRevisionsChange(e) {
-	dispatch({type:'changed', field:'allowRevision', value:e.target.checked});
-    }
-
-    function updateGroupName(e, i) {
-	dispatch({type:'groupName', index:i, value:e.target.value});
-    }
-
-    function addNewGroup() {
-	dispatch({type:'addGroup'});
-    }
-
-    function removeGroupName(i) {
-	dispatch({type:'removeGroup', index:i});
-    }
-
-    function updateVariableName(e, i) {
-	dispatch({type:'updateVariable', index:i, field:'name', value:e.target.value});
-    }
-
-    function setSelectedType(e, i) {
-	dispatch({type:'updateVariable', index:i, field:'type', value:e.target.value});
-    }
-
-    function removeVariable(i) {
-	dispatch({type:'removeVariable', index:i});
-    }
-
-    function addLevel(i) {
-	dispatch({type:'addLevel', index:i});
-    }
-
-    function updateLevel(e, variableIndex, levelIndex) {
-	dispatch({type:'changeLevel', variableIndex:variableIndex, levelIndex:levelIndex, value:e.target.value});
-    }
-
-    function removeLevel(variableIndex, levelIndex) {
-	dispatch({type:'removeLevel', variableIndex:variableIndex, levelIndex:levelIndex, value:e.target.value});
     }
 
     async function startProtocol() {
@@ -194,34 +149,32 @@ export default function NewProtocolPanel({cancelAdding}) {
 	    <h3> New Protocol Parameters </h3>
 	    <div className="error">{errorMessage}</div>
 	    <label>Name of new protocol:</label>
-	    <input value={name} onChange={handleNameChange} />
+	    <input value={name} onChange={e => setName(e.target.value.replace(/\s/g, ''))} />
 	    <p>
 		Algorithm:
-		<input type="radio" id="balanced" name="algorithm" value="Balanced"
-		       checked={"Balanced" === specs.algorithm}
-		       onChange={handleAlgorithmChange}
-		/>
-		<label htmlFor="balanced">Balanced</label>
-		<input type="radio" id="alternating" name="algorithm" value="Alternating"
-		       checked={"Alternating" === specs.algorithm}
-		       onChange={handleAlgorithmChange}
-		/>
-		<label htmlFor="alternating">Alternating</label>
+		{ ["Balanced", "Alternating"].map( alg =>
+		    <span key={alg}>
+			<input type="radio" id={alg} name="algorithm" value={alg}
+			       checked={alg === specs.algorithm}
+			       onChange={e => dispatch({type:'changed', field:'algorithm', value:alg})}
+			/>
+			<label htmlFor={alg}>{alg}</label>
+		    </span>) }
 	    </p>
 	    <input type="checkbox" id="revision"
 		   checked={specs.allowRevision}
-		   onChange={handleAllowRevisionsChange} />
+		   onChange={e => dispatch({type:'changed', field:'allowRevision', value:e.target.checked})} />
 	    <label htmlFor="revision">Allow Revisions</label>
 	    <div>
 	    Groups:
 	    { specs.groupNames.map((grp, i) =>
 		<p key={i}>
-		    <input value={grp} onChange={ (e) => updateGroupName(e, i) } />
+		    <input value={grp} onChange={ e => dispatch({type:'groupName', index:i, value:e.target.value}) } />
 		    { (specs.groupNames.length > 2) &&
-		      <button onClick={ (e) => removeGroupName(i) }>Remove Group</button> }
+		      <button onClick={ e => dispatch({type:'removeGroup', index:i}) }>Remove Group</button> }
 		</p>
 	    ) }
-		<button onClick={addNewGroup}>Add Another Group</button>
+		<button onClick={e => dispatch({type:'addGroup'})}>Add Another Group</button>
 	    </div>
 	    <div>
 		Variables to consider for balancing:
@@ -229,8 +182,10 @@ export default function NewProtocolPanel({cancelAdding}) {
 		    specs.variableSpec.map((v, i) =>
 			<div key={i}>
 			    Name of variable:
-			    <input value={v.name} onChange={(e) => updateVariableName(e,i) }/>
-			    <select value={v.type} onChange={ e => setSelectedType(e, i) } >
+			    <input value={v.name}
+				   onChange={e => dispatch({type:'updateVariable', index:i, field:'name', value:e.target.value}) }/>
+			    <select value={v.type}
+				    onChange={ e => dispatch({type:'updateVariable', index:i, field:'type', value:e.target.value}) } >
 				<option value="continuous">continuous (numeric)</option>
 				<option value="categorical">categorical</option>
 			    </select>
@@ -239,18 +194,20 @@ export default function NewProtocolPanel({cancelAdding}) {
 				  Levels:
 			          { v.levels.map( (level, levelIndex) =>
 				      <span key={levelIndex} >
-					  <br/><input value={level} onChange={e => updateLevel(e, i, levelIndex)}/>
-					  { (v.levels.length > 2) && <button onClick={e => removeLevel(i, levelIndex)}>Remove Level</button> }
+					  <br/><input value={level}
+						      onChange={e => dispatch({type:'changeLevel', variableIndex:i, levelIndex:levelIndex, value:e.target.value})}/>
+					  { (v.levels.length > 2) &&
+					    <button onClick={e => dispatch({type:'removeLevel', variableIndex:i, levelIndex:levelIndex})}>Remove Level</button> }
 				      </span>) }
 				  <br/>
-				  <button onClick={ e => addLevel(i) }> Add Level </button>
+				  <button onClick={ e => dispatch({type:'addLevel', index:i}) }> Add Level </button>
 			      </p> }
 			    { (specs.variableSpec.length > 1) &&
-			      <button onClick={ (e) => removeVariable(i) }>Remove Variable</button> }
+			      <button onClick={ e => dispatch({type:'removeVariable', index:i}) }>Remove Variable</button> }
 			</div>
 		    )
 		}
-		<button onClick={addNewVariable}>Add Another Variable</button>
+		<button onClick={e => dispatch({type:'addVariable'})}>Add Another Variable</button>
 	    </div>
 	    { !errorMessage &&
 	      <button onClick={startProtocol}>Start</button> }
